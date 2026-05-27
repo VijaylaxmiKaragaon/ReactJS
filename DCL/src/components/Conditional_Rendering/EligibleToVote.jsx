d6433af5a7dda807debc3a1d21d120943db05dc4
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-05-27 11:12:13 +0530

    Initial commit

--- DCL/src/components/Conditional_Rendering/EligibleToVote.jsx
+++ DCL/src/components/Conditional_Rendering/EligibleToVote.jsx
@@ -3,13 +3,30 @@ import React from "react";
 function EligibleToVote() {
     let age=24;
 
-    return(
-        <>
-        {
-            (age>18)?<h1>Eligible to Vote</h1> : <h1>Not eligible to vote</h1>
-        }
-        </>
-    )
+    // return(
+    //     <>
+    //     {
+    //         //By using Ternary Operator
+    //         // (age>18)?<h1>Eligible to Vote</h1> : <h1>Not eligible to vote</h1>
+
+    //         //By using && operator
+    //         //(age>18)&&<h1>Eligible to vote</h1>
+     
+    //     }
+    //     </>
+    // )
+
+
+       //By using if-else statement
+       if(age>18){
+        return(
+            <h1>Eligible to Vote</h1>
+        )
+       } else {
+        return(
+            <h1>Not eligible to vote</h1>
+        )
+       }
 }
 
 export default EligibleToVote;
