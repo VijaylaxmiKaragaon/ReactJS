cfc2c4df0c09623c0aec12a8ac308705edae5986
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-05-27 11:18:02 +0530

    Initial commit

---
+++ DCL/src/components/May27/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+    return(
+        <>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </>
+    )
+}
+
+export default App;
