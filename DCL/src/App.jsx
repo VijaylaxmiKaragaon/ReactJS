86bbab95233a4020dcf7f0192a670b2730b59f7a
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-05-28 12:11:39 +0530

    first commit

--- DCL/src/App.jsx
+++ DCL/src/App.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import DecreaseCounter from './components/DecreaseCounter';
-import ToggleText from "./components/ToggleText";
-import StudentName from "./components/StudentName";
-import CounterApp from './components/CounterApp';
-import May132 from './components/May132';
-import May13 from './components/may13';
-import May131 from './components/may131';
-import Subcription from './components/subcription';
-import StateComp from './components/StateComp';
-import BackgroundColor from "./components/BackgroundColor";
-import CharacterCounter from "./components/ChacterCounter";
-import EvenOdd from "./components/EvenOdd";
-import FontSize from "./components/FontSize";
-import HideShow from "./components/HideShow";
-import InputField from "./components/InputField";
-import StudentDetails from "./components/StudentDetails";
-import LoginLogout from "./components/LoginLogout";
-import LikeButton from "./components/LikeButton";
-import LightToggle from "./components/LightToggle";
-import EligibleToVote from "./components/Conditional_Rendering/EligibleToVote";
+import List1 from "./components/May28/List1";
+import List2 from "./components/May28/List2";
+import List3 from "./components/May28/List3";
+// import DecreaseCounter from './components/DecreaseCounter';
+// import ToggleText from "./components/ToggleText";
+// import StudentName from "./components/StudentName";
+// import CounterApp from './components/CounterApp';
+// import May132 from './components/May132';
+// import May13 from './components/may13';
+// import May131 from './components/may131';
+// import Subcription from './components/subcription';
+// import StateComp from './components/StateComp';
+// import BackgroundColor from "./components/BackgroundColor";
+// import CharacterCounter from "./components/ChacterCounter";
+// import EvenOdd from "./components/EvenOdd";
+// import FontSize from "./components/FontSize";
+// import HideShow from "./components/HideShow";
+// import InputField from "./components/InputField";
+// import StudentDetails from "./components/StudentDetails";
+// import LoginLogout from "./components/LoginLogout";
+// import LikeButton from "./components/LikeButton";
+// import LightToggle from "./components/LightToggle";
+// import EligibleToVote from "./components/Conditional_Rendering/EligibleToVote";
+// import List from "./components/May28/List";
 
 function App() {
   return (
@@ -41,7 +45,11 @@ function App() {
       {/* <LoginLogout/> */}
       {/* <LikeButton/> */}
       {/* <LightToggle/> */}
-      <EligibleToVote/>
+      {/* <EligibleToVote/> */}
+      {/* <List/> */}
+      {/* <List1/> */}
+      {/* <List2/> */}
+      <List3/>
     </>
   );
 }
