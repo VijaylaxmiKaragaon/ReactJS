a32e5e597a2867fc56f4348b09388381b38c761f
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-06-01 11:23:50 +0530

    initial commit

--- DCL/src/App.jsx
+++ DCL/src/App.jsx
@@ -1,60 +1,19 @@
 import React from "react";
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import List1 from "./components/May28/List1";
-import List2 from "./components/May28/List2";
-import List3 from "./components/May28/List3";
 import Login from "./components/Jun1/Login";
-// import DecreaseCounter from './components/DecreaseCounter';
-// import ToggleText from "./components/ToggleText";
-// import StudentName from "./components/StudentName";
-// import CounterApp from './components/CounterApp';
-// import May132 from './components/May132';
-// import May13 from './components/may13';
-// import May131 from './components/may131';
-// import Subcription from './components/subcription';
-// import StateComp from './components/StateComp';
-// import BackgroundColor from "./components/BackgroundColor";
-// import CharacterCounter from "./components/ChacterCounter";
-// import EvenOdd from "./components/EvenOdd";
-// import FontSize from "./components/FontSize";
-// import HideShow from "./components/HideShow";
-// import InputField from "./components/InputField";
-// import StudentDetails from "./components/StudentDetails";
-// import LoginLogout from "./components/LoginLogout";
-// import LikeButton from "./components/LikeButton";
-// import LightToggle from "./components/LightToggle";
-// import EligibleToVote from "./components/Conditional_Rendering/EligibleToVote";
-// import List from "./components/May28/List";
-// import List4 from "./components/May28/List4";
+
 
 function App() {
   return (
     <>
-      {/* <StateComp/> */}
-      {/* <Subcription/> */}
-      {/* <May131/> */}
-      {/* <May132/> */}
-      {/* <CounterApp/> */}
-      {/* <DecreaseCounter/> */}
-      {/* <ToggleText/> */}
-      {/* <StudentName/> */}
-      {/* <BackgroundColor/> */}
-      {/* <CharacterCounter/> */}
-      {/* <EvenOdd/> */}
-      {/* <FontSize/> */}
-      {/* <HideShow/> */}
-      {/* <InputField/> */}
-      {/* <StudentDetails/> */}
-      {/* <LoginLogout/> */}
-      {/* <LikeButton/> */}
-      {/* <LightToggle/> */}
-      {/* <EligibleToVote/> */}
-      {/* <List/>
-      <List1/> 
-      <List2/> 
-      <List3/> */}
-      {/* <List4/> */}
-      <Login/>
+     <BrowserRouter>
+     <Routes>
+      <Route>
+        <Login/>
+      </Route>
+     </Routes>
+     </BrowserRouter>
+     
     </>
   );
 }
