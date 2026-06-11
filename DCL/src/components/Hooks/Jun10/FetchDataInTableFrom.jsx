7134f78d036f0c50ee5bc2a6d3ccc00c0bd4df54
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-06-11 11:20:12 +0530

    initial commit

--- DCL/src/components/Hooks/Jun10/FetchDataInTableFrom.jsx
+++ DCL/src/components/Hooks/Jun10/FetchDataInTableFrom.jsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function FetchDataInTableFrom() {
-    const[users,setUsers] = useState([])
-    useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response=>response.json())
-        .then(data=>setUsers(data))
-    })
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  });
   return (
     <div>
       {
         <table border="1" cellPadding="10">
-            <tr>
-                <td>id</td>
-                <td>name</td>
-                <td>Email</td>
-                <td>Phone</td>
-            </tr>
-            <tr>
-            </tr>
+          <thead>
+            <td>id</td>
+            <td>name</td>
+            <td>Email</td>
+            <td>Phone</td>
+          </thead>
+          <tbody>
+           {
+             users.map((user,index)=>{
+             return  <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                </tr>
+            })
+           }
+            
+          </tbody>
         </table>
       }
     </div>
-  )
+  );
 }
 
-export default FetchDataInTableFrom
+export default FetchDataInTableFrom;
